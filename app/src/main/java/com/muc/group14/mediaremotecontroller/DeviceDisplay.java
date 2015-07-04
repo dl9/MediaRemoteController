0a837172a1e96f7edb1a554f17b29b21146b2d21
dl9 <david.lehr@uni-ulm.de>
2015-07-04 14:32:19 +0200

    Add DeviceDisplay

---
+++ app/src/main/java/com/muc/group14/mediaremotecontroller/DeviceDisplay.java
@@ -0,0 +1,41 @@
+package com.muc.group14.mediaremotecontroller;
+
+import org.fourthline.cling.model.meta.Device;
+
+/**
+ * Created by David on 01.07.2015.
+ */
+public class DeviceDisplay {
+    Device device;
+
+    public DeviceDisplay(Device device) {
+        this.device = device;
+    }
+
+    public Device getDevice() {
+        return device;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeviceDisplay that = (DeviceDisplay) o;
+        return device.equals(that.device);
+    }
+
+    @Override
+    public int hashCode() {
+        return device.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        String name =
+                getDevice().getDetails() != null && getDevice().getDetails().getFriendlyName() != null
+                        ? getDevice().getDetails().getFriendlyName()
+                        : getDevice().getDisplayString();
+        // Display a little star while the device is being loaded (see performance optimization earlier)
+        return device.isFullyHydrated() ? name : name + " *";
+    }
+}
